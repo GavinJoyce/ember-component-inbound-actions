2823f090b4e1e626066e3dfa403de1f0016e9211
Gavin Joyce <gavinjoyce@gmail.com>
2015-05-18 16:02:16 +0100

    ember 1.13.beta

--- addon/inbound-actions.js
+++ addon/inbound-actions.js
@@ -3,10 +3,9 @@ import ActionProxy from './action-proxy';
 
 export default Em.Mixin.create({
   _inbound_actions_setup: function() {
-    var proxy = ActionProxy.create({
-      target: this
+    Em.run.schedule('afterRender', this, function() {
+      var proxy = ActionProxy.create({ target: this });
+      this.set('actionReceiver', proxy);
     });
-
-    this.set('actionReceiver', proxy);
   }.on('init')
 });
