8cea22ce75f4e30d2c82524b973206178c922f5a
David Barrett <david@intercom.io>
2015-12-10 12:16:53 +0000

    No longer require prototype extensions to be enabled

--- addon/inbound-actions.js
+++ addon/inbound-actions.js
@@ -2,10 +2,10 @@ import Em from 'ember';
 import ActionProxy from './action-proxy';
 
 export default Em.Mixin.create({
-  _inbound_actions_setup: function() {
+  _inbound_actions_setup: Em.on('init', function() {
     Em.run.schedule('afterRender', this, function() {
       var proxy = ActionProxy.create({ target: this });
       this.set('actionReceiver', proxy);
     });
-  }.on('init')
+  })
 });
