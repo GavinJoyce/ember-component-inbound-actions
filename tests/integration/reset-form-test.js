7f4116c339937a1df797b82dcdfd06db39006a86
Gavin Joyce <gavinjoyce@gmail.com>
2016-11-02 09:26:58 +0000

    upgrade to ember-cli 2.9.1

--- tests/integration/reset-form-test.js
+++ tests/integration/reset-form-test.js
@@ -1,32 +1,23 @@
-import Em from 'ember';
-import startApp from 'dummy/tests/helpers/start-app';
+import { test } from 'qunit';
+import moduleForAcceptance from 'dummy/tests/helpers/module-for-acceptance';
 
-var app;
+moduleForAcceptance('Acceptance tests');
 
-module('Integration', {
-  setup: function() {
-    app = startApp();
-  },
-  teardown: function() {
-    Em.run(app, 'destroy');
-  }
-});
-
-test('the component are reset', function() {
-  expect(4);
+test('the component are reset', function(assert) {
+  assert.expect(4);
 
   visit('/');
   andThen(function() {
     let name = findWithAssert('input.name').val();
     let address = findWithAssert('textarea.address').val();
-    equal(name, 'Larry David');
-    equal(address, '1 The Blade, Mudpark, Gallinteer, Bublin 16');
+    assert.equal(name, 'Larry David');
+    assert.equal(address, '1 The Blade, Mudpark, Gallinteer, Bublin 16');
   });
   click('button');
   andThen(function() {
     let name = findWithAssert('input.name').val();
     let address = findWithAssert('textarea.address').val();
-    equal(name, '', 'The name should have been reset');
-    equal(address, '', 'The address should have been reset');
+    assert.equal(name, '', 'The name should have been reset');
+    assert.equal(address, '', 'The address should have been reset');
   });
 });
